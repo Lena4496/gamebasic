5e449f6e02fb86dcf861cd3898f17f45db55255c
Lena B <benoit.lena@protonmail.com>
2020-01-12 18:38:56 +0100

    début js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const userScore = 0;
+const computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_section = document.querySelector(".score-board");
+const result_section = document.querySelector(".result");
+const rock_article = document.getElementById("r");
+const paper_article = document.getElementById("p");
+const scissors_article = document.getElementById("s");
+
